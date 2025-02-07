c1dd4d45e22129bac2abe3519937d8f89e2d5ed2
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2025-02-07 18:29:13 +0000

    chore: init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# ShutterKit
+
+The base service kit all [Shutter.social](https://shutter.social) apps are built
+on top of.
---
+++ src/service.ts
@@ -0,0 +1,51 @@
+import { prometheus } from "@hono/prometheus";
+import { type Env, Hono } from "hono";
+import type { BlankEnv } from "hono/types";
+import { Registry } from "prom-client";
+import { Gauge } from "prom-client";
+import { version as kitVersion } from '../package.json';
+
+export class Service<E extends Env = BlankEnv> {
+  public app: Hono<E>;
+  protected metricsPrefix = "shutter_";
+  public prometheus!: ReturnType<typeof prometheus>;
+  public registry!: Registry;
+
+  constructor() {
+    this.app = new Hono<E>();
+    this.setupPrometheus();
+    this.middleware();
+  }
+
+  private setupPrometheus() {
+    this.registry = new Registry();
+
+    const versionGauge = new Gauge({
+      name: 'shutterkit_version',
+      help: 'The installed version of @shutter/shutterkit.',
+      labelNames: ['version'],
+      registers: [this.registry],
+    })
+    versionGauge.labels({ version: kitVersion }).set(1);
+
+    this.prometheus = prometheus({
+      registry: this.registry,
+      collectDefaultMetrics: true,
+      prefix: this.metricsPrefix,
+    });
+  }
+
+  protected middleware() {
+    this.app.use("*", this.prometheus.registerMetrics);
+    this.app.get("/metrics", this.prometheus.printMetrics);
+  }
+
+  start(port = 3000, hostname = "0.0.0.0") {
+    return Bun.serve({
+      development: process.env.ENV === "development",
+      port,
+      hostname,
+      fetch: this.app.fetch,
+    });
+  }
+}
